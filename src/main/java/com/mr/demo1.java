014b22484d944071f9a7e8e137b7b8647aff9f56
liulang27 <1320690090@qq.com>
2023-02-25 15:51:42 +0800

    第二次提交不需要.idea

--- src/main/java/com/mr/demo1.java
+++ src/main/java/com/mr/demo1.java
@@ -1,9 +1,9 @@
 package com.mr;
 
 import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 import org.testng.annotations.Test;
 
@@ -14,7 +14,7 @@ public class demo1 {
     public void test1() throws IOException {
         String result;
         HttpGet get = new HttpGet("https://www.baidu.com");
-        HttpClient client = new DefaultHttpClient();
+        CloseableHttpClient client = HttpClients.createDefault();
         HttpResponse response = client.execute(get);
         result = EntityUtils.toString(response.getEntity());
         System.out.println(result);
